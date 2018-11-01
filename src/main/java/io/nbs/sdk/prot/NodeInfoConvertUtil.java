9e19bcbdf89ef5ca1d7fb60937699abe4abfe7f2
lanbery.cai <471127079@qq.com>
2018-11-01 17:42:13 +0800

    new file:   src/main/java/io/nbs/sdk/prot/NodeInfoConvertUtil.java

---
+++ src/main/java/io/nbs/sdk/prot/NodeInfoConvertUtil.java
@@ -0,0 +1,13 @@
+package io.nbs.sdk.prot;
+
+/**
+ * Copyright © 2015-2020 NBSChain Holdings Limited.
+ * All rights reserved.
+ *
+ * @project nbs-client4j
+ * <p>
+ * Author   : lanbery
+ * Created  : 2018/10/19
+ */
+public class NodeInfoConvertUtil {
+}
